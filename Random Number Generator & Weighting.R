33b85a2d0ae2dd5ff85a0f14b3c3786f0eeb8350
simuh34 <70760926+simuh34@users.noreply.github.com>
2024-10-08 00:10:20 +0800

    Add files via upload

---
+++ Random Number Generator & Weighting.R
@@ -0,0 +1,70 @@
+library(dplyr)
+
+# random number generator
+ranking <- 1:6974  
+select_ranking <- function(data, step = 100, n = 2) {
+  split_data <- split(data, ceiling(seq_along(data) / step))
+  result <- lapply(split_data, function(segment) {
+    if (length(segment) >= n) {
+      sample(segment, n)
+    } else {
+      sample(segment, min(length(segment), n))  
+    }
+  })
+  return(result)
+}
+selected_journal  <-  select_ranking(ranking)
+selected_journal  <-  selected_journal %>% unlist() %>% as.data.frame %>%  `names<-`(c("rank_n"))
+
+#Post-Stratification Weighting 
+##1.whole range journals 144
+total_population <- 7174
+group_size <- 100
+sample_per_group <- 2
+total_groups <- total_population %/% group_size
+remaining_samples <- total_population %% group_size
+full_group_samples <- (total_groups +1) * sample_per_group
+weight_full_group <- ((group_size/total_population) / (sample_per_group/full_group_samples))
+
+if (remaining_samples > 0) {
+  weight_partial_group <- ((remaining_samples/total_population) / (sample_per_group/full_group_samples))  
+} else {
+  weight_partial_group <- 0  
+}
+
+cat("Weight for full groups:", weight_full_group, "\n")
+cat("Weight for partial group:", weight_partial_group, "\n")
+sample_weights <- c(rep(weight_full_group, full_group_samples-2))
+
+if (remaining_samples >= 2) {
+  sample_weights <- c(sample_weights, rep(weight_partial_group, 2)) 
+}
+print(sample_weights)
+write.csv(sample_weights,"D:\\R project\\GAL\\weight_144.csv")
+
+
+##2.
+# whole range journals 140
+total_population <- 6974
+group_size <- 100
+sample_per_group <- 2
+total_groups <- total_population %/% group_size
+remaining_samples <- total_population %% group_size
+full_group_samples <- (total_groups + 1) * sample_per_group
+weight_full_group <- ((group_size/total_population) / (sample_per_group/full_group_samples))
+
+if (remaining_samples > 0) {
+  weight_partial_group <- ((remaining_samples/total_population) / (sample_per_group/full_group_samples))  
+} else {
+  weight_partial_group <- 0  
+}
+
+cat("Weight for full groups:", weight_full_group, "\n")
+cat("Weight for partial group:", weight_partial_group, "\n")
+sample_weights <- c(rep(weight_full_group, full_group_samples-2))
+
+if (remaining_samples >= 2) {
+  sample_weights <- c(sample_weights, rep(weight_partial_group, 2)) 
+}
+print(sample_weights)
+write.csv(sample_weights,"D:\\R project\\GAL\\weight_140.csv")
